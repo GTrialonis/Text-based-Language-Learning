fc3e3bfbbf8abc6253621314f8eb8621cf1fda0c
Georgios Trialonis <gtrialonis@gmail.com>
2018-12-21 21:23:42 +0200

    tests knowledge of English based on German words
    this is the same Python program as the previous knowledge testing program. Only one or two changes inside and of course a new file is needed, i.e. the 'English_Deutsch_WB.txt'

---
+++ test_GER_to_ENG_vocabulary.py
@@ -0,0 +1,43 @@
+import random
+import sys
+try: color = sys.stdout.shell
+except AttributeError: raise RuntimeError("Use IDLE")
+from decimal import Decimal
+count = 0
+coransw = 0 # correct answers set to zero
+test_question = 0 # tested questions to zero
+perc_success = 0 # percentage of success set to zero
+repeat = 0 # ask for one more word
+
+with open('English_Deutsch_WB.txt', encoding = "UTF-8") as file:
+    
+    cont = file.readlines() # reads the contents of the file
+    lgth_file = len(cont) # finds the length in lines of the file
+    while repeat <= lgth_file:
+        num = random.randint(1, lgth_file) # obtain a random number in
+                            # in the range of the length of the file length
+        chunk = cont[num] # extract a line from the lines of the file
+        indx = chunk.index('=') # find the index of the = sign inside the chunk
+        cor_word = list(chunk[(indx):]) # pick the characters to the left of the = sign
+        corword =  ''.join(cor_word) # it joints the individual characters to words
+                                     # into the correct word
+
+        word_to_transl = input("Please translate: " + (chunk[:(indx-1)])+ ' = ')
+        test_question += 1
+        if word_to_transl in corword:
+            coransw += 1
+            perc_success = round((coransw/test_question)*100,2)
+            color.write('You are right! ', "STRING")
+            print(corword)
+            print('Success rate: ', perc_success, '%')
+            print('-----------------------------------------------')
+     
+        else:
+##            perc_success = round((coransw/test_question)*100,2) # How does this affect the score?
+            color.write('The correct answer ', "COMMENT")
+            print(corword)
+            print('Success rate: ', perc_success)
+            print('-----------------------------------------------')
+
+        repeat = repeat +1
+    
